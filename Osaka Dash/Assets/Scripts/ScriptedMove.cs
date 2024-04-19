2639340f2926adc4470e0885072a7d38f36901c1
lagcom <laggingcomputer2000@gmail.com>
2024-04-19 13:41:48 -0500

    Update ScriptedMove.cs

--- Osaka Dash/Assets/Scripts/ScriptedMove.cs
+++ Osaka Dash/Assets/Scripts/ScriptedMove.cs
@@ -4,33 +4,67 @@ using UnityEngine;
 
 public class ScriptedMove : TriggerableEvent
 {
-    [SerializeField] Vector3 moveAmount;
-    Vector3 alreadyMoved, velocity;
+    [SerializeField][Tooltip("This will be the acceleration if there is a RigidBody component")] Vector3 moveAmount;
+    Vector3 alreadyMoved, velocity, originalPosition;
     [SerializeField] float moveDuration = 1;
+    [SerializeField][Tooltip("This will not be used if there is no RigidBody component")] Vector3 startVelocity;
 
     Transform transform;
+    Rigidbody2D rigidbody2D;
+    Rigidbody rigidbody;
 
-    bool flag = false;
+    bool flag = false, useAccel;
+    float timer;
     
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<Transform>();
-        velocity = moveAmount / moveDuration;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody>();
+        useAccel = rigidbody2D == null && rigidbody == null;
+        velocity = useAccel ? (moveAmount / moveDuration) : startVelocity;
+        originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 thisFrameMovement = velocity * Time.deltaTime * (flag ? 1 : -1);
-        if (thisFrameMovement.magnitude >= ((flag?moveAmount:Vector3.zero)-alreadyMoved).magnitude)
+        if (useAccel)
         {
-
+            if (!flag)
+            {
+                timer = 0;
+                return;
+            }
+            timer += Time.deltaTime;
+            if (timer >= moveDuration) flag = false;
+            if (rigidbody == null) rigidbody2D.AddForce(moveAmount, ForceMode2D.Impulse);
+            else rigidbody.AddForce(moveAmount, ForceMode.Impulse);
+        }
+        else
+        {
+            Vector3 thisFrameMovement = velocity * Time.deltaTime * (flag ? 1 : -1);
+            if (thisFrameMovement.magnitude >= (flag ? moveAmount - alreadyMoved : alreadyMoved).magnitude)
+            {
+                alreadyMoved = flag ? moveAmount : alreadyMoved;
+                ReturnToDialogue();
+            }
+            else
+            {
+                alreadyMoved += thisFrameMovement * (flag ? 1 : -1);
+            }
+            transform.position = originalPosition + alreadyMoved;
         }
     }
 
     public override void Trigger()
     {
         flag = !flag;
+        if (useAccel)
+        {
+            if (rigidbody == null) rigidbody2D.velocity = velocity;
+            else rigidbody.velocity = velocity;
+        }
     }
 }
