3e6304af74db54f02fa8b27de9210be306f9ef18
lagcom <laggingcomputer2000@gmail.com>
2024-05-07 13:21:23 -0500

    Update opening cutscene

---
+++ Osaka Dash/Assets/Scripts/TrainDepartureTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainDepartureTrigger : MonoBehaviour
+{
+    public float characterDisappearTimer = 3f;
+    bool removeChar = false;
+    GameObject playerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!removeChar) return;
+        if (GlobalEventSystem.isInDialogue) return;
+        characterDisappearTimer -= Time.deltaTime;
+        if (characterDisappearTimer < 0) playerCharacter.SetActive(false);
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerCharacter = other.gameObject;
+            Destroy(playerCharacter.GetComponent<Collider2D>());
+            Destroy(playerCharacter.GetComponent<PlayerMovement>());
+            DialogueSystem.instance.TriggerDialogue("Train Depart");
+            removeChar = true;
+        }
+    }
+}
